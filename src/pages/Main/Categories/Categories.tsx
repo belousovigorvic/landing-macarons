d51ba1906b25b9613d5deaf47a89869c659fda82
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-07-11 23:02:50 +0600

    asdf

--- src/pages/Main/Categories/Categories.tsx
+++ src/pages/Main/Categories/Categories.tsx
@@ -12,6 +12,7 @@ import img6 from './img/img6.svg'
 const Categories: React.FC = () => {
 
 	interface dataCard {
+		id: number
 		img: string
 		title: string
 		subTitle: string
@@ -20,36 +21,42 @@ const Categories: React.FC = () => {
 
 	const dataCard: dataCard[] = [
 		{
+			id: 1,
 			img: img1,
 			title: 'Готовые наборы',
 			subTitle: 'Готовые наборы со скидкой. Вы можете подобрать набор на подходящий случай.',
 			class: 'color1'
 		},
 		{
+			id: 2,
 			img: img2,
 			title: 'Собрать свой набор',
 			subTitle: 'Выбрать количество макарун, и выбрать вкусы',
 			class: 'color2'
 		},
 		{
+			id: 3,
 			img: img3,
 			title: 'Набор с индивидуальной печатью',
 			subTitle: 'Собрать набор макарон с уникальным дизайном. ',
 			class: 'color3'
 		},
 		{
+			id: 4,
 			img: img4,
 			title: 'Свадебные предложения',
 			subTitle: 'Нежные пирожные макаронс с разными вкусами для украшения вашего свадебного торжества',
 			class: 'color4'
 		},
 		{
+			id: 5,
 			img: img5,
 			title: 'Корпоративные подарки',
 			subTitle: 'От 85 руб за шт. С уникальным дизайном. Приятный комплимент для коллег и партнеров',
 			class: 'color5'
 		},
 		{
+			id: 6,
 			img: img6,
 			title: 'Оптовые поставки',
 			subTitle: 'Предложение для кофеен, кафе, отелей и т.д. Посмотрите условия сотрудничества и отзывы.',
@@ -61,7 +68,7 @@ const Categories: React.FC = () => {
 		<div className={classes.categories}>
 			<Container>
 				<div className={classes.card__wrapper}>
-					{dataCard.map(card => <Card img={card.img} title={card.title} subTitle={card.subTitle} bg={card.class}/>)}
+					{dataCard.map(card => <Card key={card.id} img={card.img} title={card.title} subTitle={card.subTitle} bg={card.class}/>)}
 				</div>
 			</Container>
 		</div>
